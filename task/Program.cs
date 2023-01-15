547d57409a58c969ccd5e6f84d1875bd4cd36c56
Denis <lost.ned@gmail.com>
2023-01-15 16:18:12 +0300

    Added part of task

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] mainArray = new string[5] { "144", "geek", "home", "13", "qwerty"};
+string[] newArray = new string[mainArray.Length];
+
+void FillNewArray(string[] mainArray, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < mainArray.Length; i++)
+    {
+        if(mainArray[i].Length <= 3)
+        {
+            newArray[count] = mainArray[i];
+            count++;
+        }
+    }
+}
